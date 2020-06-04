5f4c8dc91365800efd93f3fa2786fd86b24d71aa
nianqi0802 <nianqi_zhang@126.com>
2020-06-04 14:14:53 -0400

    refine search

--- static/sketch.js
+++ static/sketch.js
@@ -1,26 +1,21 @@
-var weibodata;
-var searchresult;
 
-
-function preload(){
-    weibodata = loadJSON("fengchengdiary.json");
-}
-
-function setup(){
-  searchresult = weibodata[0]['Weibo'];
-
-}
-
-function draw(){
-  
-}
-
-
+var searchresult=[];
 
 
 function SearchWord(){
   var searchword = document.getElementById("searchkeyword").value;
   console.log(searchword);
-  document.getElementById("searchresult").innerHTML = searchresult;
+  searchresult =[];
+  for(let i =0;i<weibodatabase.length;i++){
+    var yeshere = weibodatabase[i]['Weibo'].search(searchword);
+    if(yeshere!=-1){
+      searchresult.push(weibodatabase[i]['Weibo']);
+    }
+    
+    
+  }
+  
+  document.getElementById("amount").innerHTML = "共搜索到"+ searchresult.length +"篇";
+  document.getElementById("searchresult").innerHTML = searchresult.join('<br><br>');
   
 }
